9b8575ddb29d95e9d720c4ba2d0673417cb388c3
andre-luizz <andreluizdefreitas0@gmail.com>
2021-09-29 18:53:01 -0300

    chore: delete lines from app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Heading from './components/Heading';
 import GlobalStyles from './styles/GlobalStyles';
 
 const App: React.FC = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<Heading>Create your account</Heading>
 		</>
 	);
 };
