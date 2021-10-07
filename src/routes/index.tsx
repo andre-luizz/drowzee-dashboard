f91b69bf7d886ad64baf80160be36883ebddd292
andre-luizz <andreluizdefreitas0@gmail.com>
2021-10-07 18:25:07 -0300

    feat: create routes file

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+const Router: React.FC = () => {
+	return (
+		<Switch>
+			<Route exact path="" />
+		</Switch>
+	);
+};
+
+export default Router;
