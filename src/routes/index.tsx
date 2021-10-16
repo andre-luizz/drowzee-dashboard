28024e6bbb2cb261a45790932f691947adba4727
andre-luizz <andreluizdefreitas0@gmail.com>
2021-10-16 15:17:50 -0300

    feat: create private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 const Router: React.FC = () => {
 	return (
 		<Switch>
 			<Route exact path="/" component={SignUp} />
 			<Route path="/sign-in" component={SignIn} />
+
+			<Route path="/dashboard" component={Dashboard} isPrivate />
 		</Switch>
 	);
 };
